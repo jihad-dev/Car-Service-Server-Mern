a3c095b44d82cf4d51e66c63b503dfa7d08a0d82
jihad <mdjihadislam166@gmail.com>
2024-02-15 11:43:48 +0600

    first

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ipn6sc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const servicesCollection = client.db("CarServiceDb").collection("services");
+    const usersCollection = client.db("CarServiceDb").collection("users");
+    const cartCollection = client.db("CarServiceDb").collection("cart");
+
+
+    // JWT Authorization //
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "29 days",
+      });
+      res.send({ token });
+    });
+    // jwt token middleware //
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+
+
+
+
+
+
+    // GET ALL Services data //
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const result = await servicesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // SAVE ALL USER INFORMATION IN THE DATABASE //
+
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const query = { email: users?.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
+
+    //  GET ALL USERS DATA AND SHOW ALL USERS ROUTE //
+    app.get("/users", verifyToken, async (req, res) => {
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // POST CART DATA TO THE cart COLLECTION //
+    app.post("/cart", async (req, res) => {
+      const cart = req.body;
+      const result = await cartCollection.insertOne(cart);
+      res.send(result);
+    });
+    // POST CART DATA GET  TO THE DATABASE //
+
+    app.get("/cart", async (req, res) => {
+      let email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World! car service !!!!!!!!!!!!!!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
